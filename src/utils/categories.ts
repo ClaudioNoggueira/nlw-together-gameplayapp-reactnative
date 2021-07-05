dec7c6cc63db3f318769b69c26d4821811917948
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-07-05 20:21:59 -0300

    Category and CategorySelect components

---
+++ src/utils/categories.ts
@@ -0,0 +1,11 @@
+import RanqueadaSVG from '../assets/ranked.svg';
+import DueloSVG from '../assets/duel.svg';
+import FunSVG from '../assets/fun.svg';
+import TrainingSVG from '../assets/training.svg';
+
+export const categories = [
+    { id: '1', title: 'Ranqueada', icon: RanqueadaSVG },
+    { id: '2', title: 'Duelo 1x1', icon: DueloSVG },
+    { id: '3', title: 'Diversão', icon: FunSVG },
+    { id: '4', title: 'Treino', icon: TrainingSVG }
+]
